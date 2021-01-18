d424bc31ca9d43e9542078be49391e97fd3ffc7e
Puspendu Biswas Paul <59274624+PuspenduPaul@users.noreply.github.com>
2021-01-18 13:37:53 +0600

    Update Calculator.py
    Used Class in this update.

--- Calculator.py
+++ Calculator.py
@@ -1,48 +1,53 @@
-def add(x,y):
-    return x + y
-def substract(x,y):
-    return x - y
-def multiply(x,y):
-    return x * y
-def divide(x,y):
-    return x / y
-def exponential(x,y):
-    return x ** y
-def Modulus(x,y):
-    return x % y
-def floor_division(x,y):
-    return x // y
+class Calculator():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-print('1.Add ')
-print('2.Substract ')
-print('3.Multiply ')
-print('4.Divide ')
-print('5.Exponential ')
-print('6.Modulus ')
-print('7.Floor Divide ')
+    def add(self, x, y):
+        return x + y
 
-choice = float(input("Choose an option(1/2/3/4/5/6/7): "))
+    def substract(self, x, y):
+        return x - y
 
-print('I')
-print('I')
-print('I')
+    def multiply(self, x, y):
+        return x * y
 
-num_1 = float(input('Give the first number: '))
-num_2 = float(input('Give the second number: '))
+    def divide(self, x, y):
+        return x / y
 
+    def exponential(self, x, y):
+        return x ** y
+
+    def Modulus(self, x, y):
+        return x % y
+
+    def floor_division(self, x, y):
+        return x // y
+
+
+print("1. Add ",
+      "2. Substract ",
+      "3. Multiply ",
+      "4. Divide ",
+      "5. Exponent ",
+      "6. Modulus ",
+      "7. Floor division ")
+choice = int(input("Give a value: "))
+
+num = Calculator(float(input()), float(input()))
 if choice == 1:
-    print(num_1, "+", num_2, "=",add(num_1,num_2) )
+    print(f"{num.x} + {num.y} = {num.add(num.x, num.y)}")
 elif choice == 2:
-    print(num_1, "-", num_2, "=", substract(num_1, num_2))
+    print(f"{num.x} - {num.y} = {num.substract(num.x, num.y)}")
 elif choice == 3:
-    print(num_1, "*", num_2, "=", multiply(num_1, num_2))
+    print(f"{num.x} * {num.y} = {num.multiply(num.x, num.y)}")
 elif choice == 4:
-    print(num_1, "/", num_2, "=", divide(num_1, num_2))
+    print(f"{num.x} / {num.y} = {num.divide(num.x, num.y)}")
 elif choice == 5:
-    print(num_1, "**", num_2, "=", exponential(num_1, num_2))
+    print(f"{num.x} ** {num.y} = {num.exponential(num.x, num.y)}")
 elif choice == 6:
-    print(num_1, "%", num_2, "=", Modulus(num_1, num_2))
+    print(f"{num.x} % {num.y} = {num.Modulus(num.x, num.y)}")
 elif choice == 7:
-    print(num_1, "//", num_2, "=", floor_division(num_1, num_2))
+    print(f"{num.x} // {num.y} = {num.floor_division(num.x, num.y)}")
 else:
-    print('Syntax Error ')
+    print("Syntax error")
